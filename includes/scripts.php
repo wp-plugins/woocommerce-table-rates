85f5a1657906b482f5c9340dde94362fe6c6ca20
rpletcher <rpletcher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-30 04:32:52 +0000

    Updating to version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-table-rates/tags/1.4.0@1190015 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/scripts.php
+++ includes/scripts.php
@@ -19,22 +19,27 @@ if( !defined( 'ABSPATH' ) ) exit;
  * @global      string $post_type The type of post that we are editing
  * @return      void
  */
-function wpr_ptr_admin_scripts( $hook ) {
-    global $edd_settings_page, $post_type;
-
-    // Use minified libraries if SCRIPT_DEBUG is turned off
-	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+function wpr_table_rate_admin_scripts( $hook ) {
+    global $wptr_settings_page, $post_type;
 
     /**
      * @todo		This block loads styles or scripts explicitly on the
      *				EDD settings page.
      */
-    if( $hook == $edd_settings_page ) {
-        wp_enqueue_script( 'wpr_ptr_admin_js', WPR_PTR_URL . '/assets/js/admin' . $suffix . '.js', array( 'jquery' ) );
-        wp_enqueue_style( 'wpr_ptr_admin_css', WPR_PTR_URL . '/assets/css/admin' . $suffix . '.css' );
+    if ($hook == 'woocommerce_page_wc-settings') {
+
+        wp_enqueue_script( 'jquery' );
+        wp_enqueue_script( 'jquery-ui-core' );
+
+        wp_enqueue_style( 'thickbox' ); // call to media files in wp
+        wp_enqueue_script( 'thickbox' );
+        wp_enqueue_script( 'media-upload' ); 
+
+        wp_enqueue_script( 'wpr_table_rate_admin_js', WPTR_SHIPPING_URL . 'assets/js/admin.js', array( 'jquery' ) );
+        wp_enqueue_style( 'wpr_table_rate_admin_css', WPTR_SHIPPING_URL . 'assets/css/admin.css' );
     }
 }
-add_action( 'admin_enqueue_scripts', 'wpr_ptr_admin_scripts', 100 );
+add_action( 'admin_enqueue_scripts', 'wpr_table_rate_admin_scripts', 100 );
 
 
 /**
@@ -43,11 +48,9 @@ add_action( 'admin_enqueue_scripts', 'wpr_ptr_admin_scripts', 100 );
  * @since       1.0.0
  * @return      void
  */
-function wpr_ptr_scripts( $hook ) {
-    // Use minified libraries if SCRIPT_DEBUG is turned off
-	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+function wpr_table_rate_scripts( $hook ) {
 
-    wp_enqueue_script( 'wpr_ptr_js', WPR_PTR_URL . '/assets/js/scripts' . $suffix . '.js', array( 'jquery' ) );
-    wp_enqueue_style( 'wpr_ptr_css', WPR_PTR_URL . '/assets/css/styles' . $suffix . '.css' );
+    wp_enqueue_script( 'wpr_table_ratejs', WPTR_SHIPPING_URL . 'assets/js/scripts.js', array( 'jquery' ) );
+    wp_enqueue_style( 'wpr_table_ratecss', WPTR_SHIPPING_URL . 'assets/css/styles.css' );
 }
-add_action( 'wp_enqueue_scripts', 'wpr_ptr_scripts' );
+add_action( 'wp_enqueue_scripts', 'wpr_table_rate_scripts' );
